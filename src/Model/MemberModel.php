a09056d360a2e5e32b5b499c6c0521c222cfe39e
Maksym Tymchyk <reilag69@gmail.com>
2016-06-25 12:40:33 +0300

    Ready to use

--- src/Model/MemberModel.php
+++ src/Model/MemberModel.php
@@ -13,26 +13,6 @@ use Kuna\Service\PrivateRequest;
 class MemberModel extends PrivateModel
 {
 
-	/**
-	 * @return array|null
-	 */
-	public function me()
-	{
-		$request = new PrivateRequest("members/me");
-		$result = $this->client->execute($request);
-		return $result;
-	}
-
-	/**
-	 * @return array|null
-	 */
-	public function trades($market = Constant::MARKET_BTCUAH)
-	{
-		$request = new PrivateRequest("trades/my", [
-			'market' => $market
-		]);
-		$result = $this->client->execute($request);
-		return $result;
-	}
+	
 
 }
